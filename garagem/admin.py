99246c58bd9f641329921354df22bb0eb62a3c0b
eliaswhite <souzabrancoelias8@gmail.com>
2023-08-16 20:20:17 -0300

    Criação das models Acessorio e cor

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-
-from django.contrib import admin
-
-from .models import Marca, Categoria
+from .models import Marca, Categoria, Cor, Acessorio
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Acessorio)
+admin.site.register(Cor)
