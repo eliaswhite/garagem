a40a8eba13c0ba77c8679b9ba29a7deb800d5cc2
eliaswhite <souzabrancoelias8@gmail.com>
2023-08-16 21:03:18 -0300

    Adicionando Modelo no admin

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Marca, Categoria, Cor, Acessorio, Veiculo
+from .models import Marca, Categoria, Cor, Acessorio, Veiculo, Modelo
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
 admin.site.register(Acessorio)
 admin.site.register(Cor)
 admin.site.register(Veiculo)
+admin.site.register(Modelo)
